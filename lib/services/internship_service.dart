2fa6e6e8058dae79f3d4f8eed9e99552b5c38c07
HarshitSinha1008 <harshitsinha55@gmail.com>
2026-06-22 00:55:42 +0530

    internship service completed

--- lib/services/internship_service.dart
+++ lib/services/internship_service.dart
@@ -1,9 +1,27 @@
 import 'package:hive/hive.dart';
 import 'package:interntrack/models/internship_models.dart';
 
+class InternshipService {
+
 List<InternshipModel> getAllInternships() {
   final box = Hive.box<InternshipModel>('internships');
   final list = box.values.toList(); // box.value gives you all stored object, .toList() converts it to list
   list.sort((a, b) => b.dateApplied.compareTo(a.dateApplied)); // sort by dateApplied newest first
   return list;
+}
+
+Future<void> addInternship(InternshipModel internship) async {
+  final box = Hive.box<InternshipModel>('internships');
+  return box.put(internship.id, internship);
+}
+
+Future<void> updateInternship(InternshipModel internship) async {
+  final box = Hive.box<InternshipModel>('internships');
+  return box.put(internship.id, internship);
+}
+
+Future<void> deletePost(String id) async {
+    final box = Hive.box<InternshipModel>('internships');
+    await box.delete(id);
+  }
 }
