f4207db6d31c96a6b1bb34ae641b6358b744b480
HarshitSinha1008 <harshitsinha55@gmail.com>
2026-06-24 01:24:28 +0530

    started add edit screen

--- lib/screens/add_edit_screen.dart
+++ lib/screens/add_edit_screen.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:interntrack/services/internship_service.dart';
+import 'package:interntrack/models/internship_models.dart';
 
-class AddEditScreen extends StatelessWidget {
-  const AddEditScreen({super.key});
+class AddEditScreen extends StatefulWidget {
+  final InternshipModel? internship;
+  const AddEditScreen({super.key, this.internship});
+
+  @override
+  State<AddEditScreen> createState() => _AddEditScreenState();
+}
+
+class _AddEditScreenState extends State<AddEditScreen> {
+
+  final _companyController = TextEditingController();
+  final _roleController = TextEditingController();
+  String _selectedStatus = 'Applied';
+  bool _isSaving = false;
+  final _notesController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.internship != null) {
+      _companyController.text = widget.internship!.companyName;
+      _roleController.text = widget.internship!.role;
+      _selectedStatus = widget.internship!.status;
+      _selectedDate = widget.internship!.dateApplied;
+      _notesController.text = widget.internship!.notes;
+    }
+  }
+
+  @override
+  void dispose() {
+    _companyController.dispose();
+    _roleController.dispose();
+    _notesController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _save() async {
+    // validating fields
+    if (_companyController.text.isEmpty || _roleController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill in all fields.')),
+      );
+      return;
+    }
+
+    setState(() => _isSaving = true);
+
+    final internship = InternshipModel(
+      id: widget.internship?.id ?? DateTime.now().toString(),
+      companyName: _companyController.text,
+      role: _roleController.text,
+      status: _selectedStatus,
+      dateApplied: _selectedDate,
+      notes: _notesController.text,
+    );
+
+    try {
+      if (widget.internship != null) {
+        await InternshipService().updateInternship(internship);
+      } else {
+        await InternshipService().addInternship(internship);
+      }
+
+      if (mounted) Navigator.pop(context, true);}
+      catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to save internship.')),
+        );
+      }finally {
+        setState(() => _isSaving = false);
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
